c135384cde3791f412ce1715097f91d65b0bf0dd
Yogesh Yashvant Nikam <yogeshnikam9900@gmail.com>
2020-06-24 13:48:04 +0530

    Update init_linkedin_login.php
    Added comments

--- publichtml/init_linkedin_login.php
+++ publichtml/init_linkedin_login.php
@@ -4,8 +4,8 @@ use yogesh\LinkedIn;
 
 require_once "LinkedIn.php";
 
-$app_id = "";
-$app_secret = "";
+$app_id = ""; //Add your app id
+$app_secret = ""; //Add your app secret here
 $callback = "http://localhost/linkedin_callback.php";
 $scopes = "r_emailaddress r_liteprofile";
 $ssl = false; //TRUE FOR PRODUCTION ENV.
